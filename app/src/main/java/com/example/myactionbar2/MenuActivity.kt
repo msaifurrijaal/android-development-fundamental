b4de20eb303b6d2c4ad2637bee31baab2b088252
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2023-01-08 11:27:08 +0700

    add search view on action bar

--- app/src/main/java/com/example/myactionbar2/MenuActivity.kt
+++ app/src/main/java/com/example/myactionbar2/MenuActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myactionbar2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.myactionbar2.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
