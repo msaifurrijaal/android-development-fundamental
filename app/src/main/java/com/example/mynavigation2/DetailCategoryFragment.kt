17011085012112959c85ee467e52a115d000c2a8
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2023-01-08 10:19:46 +0700

    latihan navigation

---
+++ app/src/main/java/com/example/mynavigation2/DetailCategoryFragment.kt
@@ -0,0 +1,47 @@
+package com.example.mynavigation2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.mynavigation2.databinding.FragmentDetailCategoryBinding
+
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener {
+            view.findNavController().navigate(DetailCategoryFragmentDirections.actionDetailCategoryFragmentToHomeFragment())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
