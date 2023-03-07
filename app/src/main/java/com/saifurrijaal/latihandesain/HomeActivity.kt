19efe14d7823a8e121a1d72af48c05fccd8961ff
msaifurrijaal <msaifurrijaal@gmail.com>
2023-03-08 02:41:34 +0700

    first commit

---
+++ app/src/main/java/com/saifurrijaal/latihandesain/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.saifurrijaal.latihandesain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/saifurrijaal/latihandesain/SplashTravelActivity.kt
@@ -0,0 +1,11 @@
+package com.saifurrijaal.latihandesain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashTravelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_travel)
+    }
+}
